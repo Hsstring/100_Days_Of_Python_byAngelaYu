067b0b4f3b1a93a81cf6f9276fe69922742bf23a
Hsstring <94295693+Hsstring@users.noreply.github.com>
2025-03-12 09:33:12 +0330

    Add files via upload

---
+++ Day 16/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+import another_module
+print(another_module.another_variable)
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkSeagreen2")
+timmy.forward(100)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
